35f963dd9cf3e560abf64a6017cc691a8b78fef1
Ben Sukstorf <bsukstorf@gmail.com>
2022-07-24 14:06:04 +0200

    Gently add shadow to hamburger to improve visibility

--- components/menu-icon.vue
+++ components/menu-icon.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <svg viewBox="0 0 100 80" width="40" height="40">
-      <rect width="100" height="12"></rect>
-      <rect y="30" width="100" height="12"></rect>
-      <rect y="60" width="100" height="12"></rect>
+    <svg viewBox="0 0 100 80" width="40" height="40" class="shadow">
+      <g stroke="none" stroke-width="1" fill="black" fill-rule="nonzero">
+        <rect width="100" height="12"></rect>
+        <rect y="30" width="100" height="12"></rect>
+        <rect y="60" width="100" height="12"></rect>
+      </g>
     </svg>
   </div>
 </template>
@@ -16,5 +18,11 @@ svg {
 
 div {
   padding: 0 1rem;
+}
+
+.shadow {
+  -webkit-filter: drop-shadow(3px 3px 2px rgba(165, 165, 165, 0.7));
+  filter: drop-shadow(3px 3px 2px rgba(165, 165, 165, 0.7));
+  /* Similar syntax to box-shadow */
 }
 </style>
