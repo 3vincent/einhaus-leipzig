e43712595f59e23bae81e8de4f66703913919a49
Ben Sukstorf <bsukstorf@gmail.com>
2022-10-07 20:19:40 +0200

    Disable static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 import { defineNuxtConfig } from 'nuxt/config'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static', // default is 'server'
+  // target: 'static', // default is 'server'
   css: ['~/assets/styles/index.scss'],
 })
