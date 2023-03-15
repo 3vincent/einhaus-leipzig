1e3724aaf83a9768785217c9a9ede37129b44101
Ben Sukstorf <bsukstorf@gmail.com>
2023-03-16 00:23:09 +0100

    Replace scrollEventHandler with Intersection Observer (for performance)

--- components/landing/unterstutzung.vue
+++ components/landing/unterstutzung.vue
@@ -1,42 +1,49 @@
 <template>
-  <div id="unterstuetzung" class="landing-container background lastContainer">
-    <div class="landing-contentBox">
-      <h2>Unterstütze unser Projekt.</h2>
-      <p>
-        Den Kauf haben wir mit unserem eigenen Kapital und mit der Hilfe
-        zahlreicher Unterstützer:innen, sowie einem Bankkredit von der
-        Umweltbank realisiert. Unsere Unterstützer:innen helfen uns den
-        Bankkredit möglichst gering zu halten bzw. durch Sondertilgungen
-        schneller zurückzahlen zu können. Und hier kommst Du ins Spiel. Werde
-        investierendes Mitglied unserer Genossenschaft! Das ist bereits mit
-        einem Betrag von 100 Euro möglich. Es gibt zwei Wege, wie Du uns
-        finanziell unterstützen kannst: indem Du Genossenschaftsanteile erwirbst
-        oder indem Du der Genossenschaft einen Direktkredit* gewährst.
-      </p>
+  <div>
+    <div class="intersection-observer-alert-element-top"></div>
 
-      <p>
-        Für dich ist diese Art der Investition attraktiv, wenn du dein Geld
-        gerne in einem Projekt anlegst, das man anfassen kann: Dein Geld kannst
-        du bei uns nachhaltig anlegen. Mehr Informationen geben wir Dir gerne
-        persönlich. Wir freuen uns über eine E-Mail an
-        {{ envVar.public.OFFICIAL_CONTACT_ADDRESS }}
-      </p>
+    <div id="unterstuetzung" class="landing-container background lastContainer">
+      <div class="landing-contentBox">
+        <h2>Unterstütze unser Projekt.</h2>
+        <p>
+          Den Kauf haben wir mit unserem eigenen Kapital und mit der Hilfe
+          zahlreicher Unterstützer:innen, sowie einem Bankkredit von der
+          Umweltbank realisiert. Unsere Unterstützer:innen helfen uns den
+          Bankkredit möglichst gering zu halten bzw. durch Sondertilgungen
+          schneller zurückzahlen zu können. Und hier kommst Du ins Spiel. Werde
+          investierendes Mitglied unserer Genossenschaft! Das ist bereits mit
+          einem Betrag von 100 Euro möglich. Es gibt zwei Wege, wie Du uns
+          finanziell unterstützen kannst: indem Du Genossenschaftsanteile
+          erwirbst oder indem Du der Genossenschaft einen Direktkredit*
+          gewährst.
+        </p>
 
-      <NuxtLink to="/kontakt">
-        <button class="link primary"> Kontaktformular </button>
-      </NuxtLink>
+        <p>
+          Für dich ist diese Art der Investition attraktiv, wenn du dein Geld
+          gerne in einem Projekt anlegst, das man anfassen kann: Dein Geld
+          kannst du bei uns nachhaltig anlegen. Mehr Informationen geben wir Dir
+          gerne persönlich. Wir freuen uns über eine E-Mail an
+          {{ envVar.public.OFFICIAL_CONTACT_ADDRESS }}
+        </p>
 
-      <a :href="`mailto:${envVar.public.OFFICIAL_CONTACT_ADDRESS}`">
-        <button class="link secondary"> Email schreiben </button>
-      </a>
+        <NuxtLink to="/kontakt">
+          <button class="link primary"> Kontaktformular </button>
+        </NuxtLink>
 
-      <p>
-        <small
-          >*) Bei den Direktkrediten handelt es sich um Nachrangdarlehen, für
-          die keine Prospektpflicht besteht.</small
-        >
-      </p>
+        <a :href="`mailto:${envVar.public.OFFICIAL_CONTACT_ADDRESS}`">
+          <button class="link secondary"> Email schreiben </button>
+        </a>
+
+        <p>
+          <small>
+            *) Bei den Direktkrediten handelt es sich um Nachrangdarlehen, für
+            die keine Prospektpflicht besteht.
+          </small>
+        </p>
+      </div>
     </div>
+
+    <div class="intersection-observer-alert-element-bottom"></div>
   </div>
 </template>
 
@@ -53,11 +60,6 @@ export default {
 
   mounted() {
     this.fixBackgroundImage()
-    window.addEventListener('scroll', debounce(this.fixBackgroundImage, 10))
-  },
-
-  beforeUnmount() {
-    window.removeEventListener('scroll', this.fixBackgroundImage)
   },
 
   methods: {
@@ -66,21 +68,32 @@ export default {
 
       const element = document.querySelector('.lastContainer') as HTMLDivElement
 
-      if (!element) return
+      const intersectioObserverAlertElementBottom = document.querySelector(
+        '.intersection-observer-alert-element-bottom'
+      ) as HTMLDivElement
 
-      const scrollPositionOfElement = element.getBoundingClientRect().y
+      const intersectioObserverAlertElementTop = document.querySelector(
+        '.intersection-observer-alert-element-top'
+      ) as HTMLDivElement
 
-      if (scrollPositionOfElement <= 0) {
-        element.classList.add('stickyBackground')
-        return
-      }
+      if ('IntersectionObserver' in window) {
+        let fixBackgroundImage = new IntersectionObserver(
+          (entries, observer) => {
+            entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                if (entry.target === intersectioObserverAlertElementTop) {
+                  element.classList.remove('stickyBackground')
+                  return
+                }
 
-      if (
-        scrollPositionOfElement > 0 &&
-        element.classList.contains('stickyBackground')
-      ) {
-        element.classList.remove('stickyBackground')
-        return
+                element.classList.add('stickyBackground')
+              }
+            })
+          }
+        )
+
+        fixBackgroundImage.observe(intersectioObserverAlertElementBottom)
+        fixBackgroundImage.observe(intersectioObserverAlertElementTop)
       }
     },
   },
@@ -109,5 +122,20 @@ small {
   font-weight: 500;
   opacity: 0.8;
   line-height: 0.1rem;
+}
+.intersection-observer-alert-element-top,
+.intersection-observer-alert-element-bottom {
+  position: absolute;
+  background-color: transparent;
+  height: 1px;
+  width: 100vw;
+}
+
+.intersection-observer-alert-element-top {
+  top: -2px;
+}
+
+.intersection-observer-alert-element-bottom {
+  bottom: -2px;
 }
 </style>
