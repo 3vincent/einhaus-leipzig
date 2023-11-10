a51a58bfd409cebe78b0c994e2db190c0e554ac8
Ben Sukstorf <bsukstorf@gmail.com>
2023-11-10 18:05:15 +0100

    Make cookie handling more transparent

--- components/cookie-delete-button.vue
+++ components/cookie-delete-button.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 const cookieConsent = useCookie('cookieConsent')
+const showCheck = ref(false)
 
 const deleteCookie = () => {
   try {
     // @ts-ignore
-    cookieConsent.value = null
+    showCheck.value = true
+    setTimeout(() => {
+      showCheck.value = false
+      cookieConsent.value = 'initial'
+    }, 1000)
     console.log('Cookie deleted..')
   } catch (error) {
     console.log(error)
@@ -18,8 +23,53 @@ const deleteCookie = () => {
     rel="nofollow"
     class="link secondary"
     v-on:click.prevent="deleteCookie"
-    v-if="cookieConsent"
+    v-if="cookieConsent !== 'initial'"
   >
     Cookie löschen
+    <Transition>
+      <div v-if="showCheck"></div>
+    </Transition>
   </a>
 </template>
+
+<style scoped lang="scss">
+a.link.secondary {
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: var(--pretty-green);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid white;
+
+    &::after {
+      content: '';
+      position: absolute;
+      height: 20px;
+      width: 10px;
+      transform: rotate(35deg);
+      border: 2px solid white;
+      border-left-width: 0;
+      border-top-width: 0;
+    }
+  }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
