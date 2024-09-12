9f155b28cfadf166996685adfcd1f9f230c6c868
Ben Sukstorf <bsukstorf@gmail.com>
2024-09-12 20:53:38 +0200

    Simplify scrollBevhavior function

--- app/router.options.ts
+++ app/router.options.ts
@@ -2,19 +2,20 @@ import type { RouterConfig } from '@nuxt/schema'
 
 export default <RouterConfig>{
   scrollBehavior(_to, _from, savedPosition) {
-    if (_to.name === 'index') {
+    return nextTick().then(() => {
       const root = document.querySelector('html')
-      if (root) root.style.scrollBehavior = 'smooth'
-    } else {
-      const root = document.querySelector('html')
-      if (root) root.style.scrollBehavior = 'auto'
-    }
 
-    if (_from.name === _to.name) return
+      if (root) {
+        root.style.scrollBehavior = _to.name === 'index' ? 'smooth' : 'auto'
+      }
 
-    if (savedPosition) {
-      return savedPosition
-    }
-    return { top: 0 }
+      if (_from.name === _to.name) return
+
+      if (savedPosition) {
+        return savedPosition
+      }
+
+      return { top: 0 }
+    })
   },
 }
