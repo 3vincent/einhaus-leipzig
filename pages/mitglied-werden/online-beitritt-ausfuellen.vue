f9591ca49e21119d59dfaf0b09f14368a9cc5120
Ben Sukstorf <bsukstorf@gmail.com>
2025-12-10 01:02:16 +0100

    Add breadcrump and page meta info

--- pages/mitglied-werden/online-beitritt-ausfuellen.vue
+++ pages/mitglied-werden/online-beitritt-ausfuellen.vue
@@ -1,3 +1,38 @@
 <template>
-  <div>Hallo</div>
+  <div class="default-container">
+    <div class="content">
+      <BreadcrumbNav />
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title:
+    'Mitglied werden - Online-Beitritt ausfüllen - EinHaus Reichpietschstraße 13 eG - Genossenschaft Leipzig',
+  description: 'Mitglied werden',
+  ogTitle:
+    'Mitglied werden - Online-Beitritt ausfüllen - EinHaus Reichpietschstraße 13 eG - Genossenschaft Leipzig',
+  ogSiteName:
+    'Mitglied werden - Online-Beitritt ausfüllen - EinHaus Reichpietschstraße 13 eG - Genossenschaft Leipzig',
+  ogImage:
+    'https://www.einhaus-leipzig.de/images/logo-einhaus-genossenschaft-leipzig.png',
+  ogDescription: 'Mitglied werden - Online-Beitritt ausfüllen',
+  ogUrl:
+    'https://www.einhaus-leipzig.de/mitglied-werden/online-beitritt-ausfuellen',
+  ogType: 'website',
+})
+
+useHead({
+  link: [
+    {
+      rel: 'canonical',
+      href: 'https://www.einhaus-leipzig.de/mitglied-werden/online-beitritt-ausfuellen',
+    },
+  ],
+})
+
+definePageMeta({
+  layout: 'default',
+})
+</script>
